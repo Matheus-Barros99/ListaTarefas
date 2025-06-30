6d37d12ff832d8a64b9e9164aacfed444561d8a3
matheus barros <matheusbarros1999@hotmail.com>
2025-06-30 11:18:25 -0300

    control e thread para apagar tarefas concluidas a mais de 10 dias

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,34 @@
-﻿namespace ListaTarefas
+﻿using Database.DbContexts;
+
+namespace ListaTarefas
 {
     public partial class App : Application
     {
-        public App()
+        private readonly DatabaseDbContext _contexto;
+        public App(DatabaseDbContext contexto)
         {
+            _contexto = contexto;
             InitializeComponent();
+
+            var timerExpurgo = Application.Current.Dispatcher.CreateTimer();
+            timerExpurgo.Interval = TimeSpan.FromMinutes(5);
+            timerExpurgo.Tick += (s, e) =>
+            {
+                MainThread.BeginInvokeOnMainThread(async () =>
+                {
+                    try
+                    {
+                        await new Controls.TarefaControl(_contexto).ExpurgarTarefas();
+                    }
+                    catch (Exception ex)
+                    {
+                        string erro = ex.Message;
+                    }
+                    finally{ }
+
+                });
+            };
+            timerExpurgo.Start();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
---
+++ Controls/TarefaControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaTarefas.Controls
+{
+    public class TarefaControl
+    {
+        private readonly Database.DbContexts.DatabaseDbContext _contexto;
+
+        public TarefaControl(Database.DbContexts.DatabaseDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task ExpurgarTarefas()
+        {
+            var listaTarefasExpurgo = _contexto.Tarefas
+                                               .Where(t => t.StatusTarefa == Database.Models.Enums.StatusTarefa.Concluida
+                                                        && t.Finalizacao <= DateTime.Now.Date.AddDays(10))
+                                               .ToList();
+
+            if (listaTarefasExpurgo.Any())
+            {
+                _contexto.Tarefas.RemoveRange(listaTarefasExpurgo);
+                await _contexto.SaveChangesAsync();
+            }
+        }
+    }
+}
